863ee4385377f15ba8a4c24c5c618ea6d0126ecf
Adam Nowicki <adamnow83@gmail.com>
2022-11-06 14:27:18 +0000

    empty functions

--- Battleships.py
+++ Battleships.py
@@ -1,4 +1,56 @@
 #BATTLESHIP GAME
 
-def get_board():
+def placement_phase(player):
+    pass
+
+def get_empty_board():
+    pass
+
+def display_board():
+    pass
+
+def valid_input():
+    pass
+
+def ship_proximity():
+    #horizontal or ventical, regardless of corners
+    pass
+
+def what_ships():
+    #creates variable with ship existing in game
+    pass
+
+def ship_placement():
+    pass
+
+
+def shooting_phase():
+    pass
+
+def ask_for_shot():
+    pass
+
+def is_shot_valid():
+    pass
+
+def hit_or_miss():
+    pass
+
+def is_ship_sunk():
+    pass
+
+def win_condition():
+    pass
+
+player1_board = get_empty_board()
+player2_board = get_empty_board()
+player1_shoots = get_empty_board()
+player2_shoots = get_empty_board()
+
+
+
+
+
+
+def main_logic():
     pass
