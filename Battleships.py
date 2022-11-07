abb07e5fa184fdf772f85c96b99f6bbeab145f7b
HLGitHuber <hubertlewandowski2002@gmail.com>
2022-11-07 16:03:04 +0100

    Optimized code

--- Battleships.py
+++ Battleships.py
@@ -46,20 +46,12 @@ def is_ship_sunk():
     pass
 
 def win_condition(player_board):
-    if is_all_ship_sunk(player_board):
-        return True
-    else:
-        return False
-
-def is_all_ship_sunk(player_board):
     board_size = len(player_board)
     for row in range(board_size):
         for col in range(board_size):
             if player_board[row][col] == 'X':
                 return False
     return True
-    
-    
 
 player1_board = get_empty_board(5)
 player2_board = get_empty_board(5)
@@ -67,13 +59,6 @@ player1_shoots = get_empty_board(5)
 player2_shoots = get_empty_board(5)
 
 
-
-
 def main_logic():
     pass
 
-# player1_board[0][0] = 'X'
-
-# print(player1_board)
-
-# print(win_condition(player1_board))
