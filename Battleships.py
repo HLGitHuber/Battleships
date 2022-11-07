f3112702439f204a8703efec6b26a8fddfcb7556
HLGitHuber <hubertlewandowski2002@gmail.com>
2022-11-07 13:01:50 +0100

    test

---
+++ Battleships.py
@@ -0,0 +1,115 @@
+#BATTLESHIP GAME
+
+def placement_phase(player):
+    pass
+
+<<<<<<< HEAD
+def get_empty_board(size):
+    return  [['O' for _ in range(size)] for _ in range(size)]
+
+def display_board(game_board):
+    alphabet = 'ABCDEFGHIJ'
+    display_size = len(game_board)
+    first_line = [str(x) for x in range(1, display_size+1)]
+    print("  " + " ".join(first_line))
+    for x in range(0, (display_size)):
+        print(f'{alphabet[x]} {" ".join(game_board[x])}')
+
+# is ship placement valid
+=======
+def get_empty_board():
+    pass
+
+def display_board():
+    pass
+
+>>>>>>> 863ee4385377f15ba8a4c24c5c618ea6d0126ecf
+def valid_input():
+    pass
+
+def ship_proximity():
+    #horizontal or ventical, regardless of corners
+    pass
+
+def what_ships():
+    #creates variable with ship existing in game
+    pass
+
+def ship_placement():
+    pass
+
+<<<<<<< HEAD
+def shooting_phase():
+    pass
+
+def ask_for_shot(): ##
+    pass
+
+def is_shot_valid(): ##
+    pass
+
+def hit_or_miss(): ##
+=======
+
+def shooting_phase():
+    pass
+
+def ask_for_shot():
+    pass
+
+def is_shot_valid():
+    pass
+
+def hit_or_miss():
+>>>>>>> 863ee4385377f15ba8a4c24c5c618ea6d0126ecf
+    pass
+
+def is_ship_sunk():
+    pass
+
+<<<<<<< HEAD
+def win_condition(player1_board, player2_board):
+    if is_all_ship_sunk(player1_board):
+        return player1_board
+    elif is_all_ship_sunk(player2_board):
+        return player2_board
+    else:
+        return False
+
+def is_all_ship_sunk(player_board):
+    board_size = len(player_board)
+    for row in range(board_size):
+        for col in range(board_size):
+            if player_board[row][col] == 'X':
+                return False
+    return True
+    
+    
+
+player1_board = get_empty_board(5)
+player2_board = get_empty_board(5)
+player1_shoots = get_empty_board(5)
+player2_shoots = get_empty_board(5)
+
+
+def main_logic():
+    pass
+
+print(win_condition(player1_board, player2_board))
+=======
+def win_condition():
+    pass
+
+player1_board = get_empty_board()
+player2_board = get_empty_board()
+player1_shoots = get_empty_board()
+player2_shoots = get_empty_board()
+
+
+
+
+
+
+def main_logic():
+    pass
+>>>>>>> 863ee4385377f15ba8a4c24c5c618ea6d0126ecf
