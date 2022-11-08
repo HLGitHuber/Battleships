915f221dc943e2b4cad5d88f67ab981247106f85
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2022-11-08 09:08:30 +0100

    Nowe funkcje
    Dodaje dwie nowe funkcje, is_shot_valid() oraz hit_or_miss(). Obie nie są skończone, nie wiem do końca jak pobrać do nich shot z poprzedniej funkcji, wymagają pracy.

--- battleship.py
+++ battleship.py
@@ -1,5 +1,30 @@
+shots = []
+board = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+ships_start = []
+ships = []
+
+
 def ask_for_shot():
-    print('Choose the place to shoot')
-    shot = input()
+    shot = input('Choose the place to shoot')
     return shot
+
+def is_shot_valid(shot):
+    if shot not in board:
+        print ("Your shot is outside the board, try again")
+        is_shot_valid(shot)
+    elif shot in shots:
+        print ("You have already shot there, try again")
+        is_shot_valid(shot)
+    else :
+        shots.append(shot)
+        return True
+
+
+def hit_or_miss(shot):
+    if shot in ships:
+        print("You hit enemy ship!")
+        ships.remove(shot)
+    else:
+        print('You missed the shot')
+
 
