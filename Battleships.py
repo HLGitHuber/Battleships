be7ca3f5cf35a661344894d617d84118db5763a4
Adam Nowicki <115307872+AdamNowicki22@users.noreply.github.com>
2022-11-13 20:59:03 +0000

    Update Battleships.py
    ship placement for size ==`
    ship direction
    adjacent empty

--- Battleships.py
+++ Battleships.py
@@ -1,10 +1,11 @@
-#BATTLESHIP GAME
+def get_empty_board(board_size):
+    return  [['O' for _ in range(board_size)] for _ in range(board_size)]
 
-def placement_phase(player):
-    pass
 
-def get_empty_board(size):
-    return  [['O' for _ in range(size)] for _ in range(size)]
+def ask_for_input(ship_size):
+    coordinates = input("Please provide position in format ie. A1, size of current ship is {ship_size}: ")
+    return coordinates
+
 
 def display_board(game_board):
     alphabet = 'ABCDEFGHIJ'
@@ -13,11 +14,6 @@ def display_board(game_board):
     print("  " + " ".join(first_line))
     for x in range(0, (display_size)):
         print(f'{alphabet[x]} {" ".join(game_board[x])}')
-
-def ask_for_input(ship_size):
-    coordinates = input("Please provide position in format ie. A1, size of current ship is {ship_size}: ")
-    return coordinates
-
 
 def is_valid_input(player_input, size):
     if len(player_input) ==2:
@@ -33,13 +29,15 @@ def is_valid_input(player_input, size):
             return True
     else:
         return False
-    
+
+
 def check_if_position_empty(player_coordinates, player_board):
-    if player_board[int(player_coordinates[0])][int(player_coordinates[1])] == 'O':
-        return True
-    else:
+    if player_board[int(player_coordinates[0])][int(player_coordinates[1])] == IndexError or player_board[int(player_coordinates[0])][int(player_coordinates[1])] != 'O':
         return False
-    
+    elif player_board[int(player_coordinates[0])][int(player_coordinates[1])] == 'O':
+        return True
+        
+
 def get_player_coordinates_format(player_coordinate):
     rows_conversion_dict = {"A": 0, "B": 1, "C": 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,'H': 7, 'I': 8, 'J': 9}
     row = rows_conversion_dict[player_coordinate[0].upper()]
@@ -48,12 +46,22 @@ def get_player_coordinates_format(player_coordinate):
     return player_coordinates_format
 
 
+
 def get_valid_input(player_coordinates, player_board, ship_size): 
     player_coordinates = ask_for_input(ship_size)
     while check_if_position_empty(player_coordinates, player_board) == False or is_valid_input(player_coordinates, ship_size) == False:
         player_coordinates = ask_for_input(ship_size)
         if check_if_position_empty(player_coordinates, player_board) == True and is_valid_input(player_coordinates, ship_size) == True:
             return get_player_coordinates_format(player_coordinates)
+
+
+
+
+
+board_player1 = get_empty_board(5)
+
+# player1_ships = [['00'], ['44'], ['22','23'], ['30', '40']]
+ships_dict = {1: 1, 2:0, 3:0, 4:0}
 
 def check_if_adjecent_empty(player_board, ship_location):
     rows = int(ship_location[0])
@@ -65,73 +73,21 @@ def check_if_adjecent_empty(player_board, ship_location):
                     return True
     return False
 
-def get_ships_dict(ships_dict):
-    player_ships = []
-    for x in range(1, (len(ships_dict)+1)): 
-        while x == 1 and ships_dict[1]>0:
-            shiplocation1 = input("please provide ship coordinates(ship size is 1) in format like A1: ")
-            single_ship = [shiplocation1]
-            player_ships.append(single_ship)
-            ships_dict[1] -=1
-        while x == 2 and ships_dict[2]>0:
-            shiplocation1 = input("please provide ship coordinates(ship size is 2) in format like A1: ")
-            shiplocation2 = input("please provide ship coordinates(ship size is 2) in format like A1: ")
-            single_ship = [shiplocation1, shiplocation2]
-            player_ships.append(single_ship)
-            ships_dict[2] -=1
-        while x == 3 and ships_dict[3]>0:
-            shiplocation1 = input("please provide ship coordinates(ship size is 3) in format like A1: ")
-            shiplocation2 = input("please provide ship coordinates(ship size is 3) in format like A1: ")
-            shiplocation3 = input("please provide ship coordinates(ship size is 3) in format like A1: ")
-            single_ship = [shiplocation1, shiplocation2,shiplocation3]
-            player_ships.append(single_ship)
-            ships_dict[3] -=1
-        while x == 4 and ships_dict[4]>0:
-            shiplocation1 = input("please provide ship coordinates(ship size is 4) in format like A1: ")
-            shiplocation2 = input("please provide ship coordinates(ship size is 4) in format like A1: ")
-            shiplocation3 = input("please provide ship coordinates(ship size is 4) in format like A1: ")
-            shiplocation4 = input("please provide ship coordinates(ship size is 4) in format like A1: ")
-            single_ship = [shiplocation1, shiplocation2,shiplocation3, shiplocation4]
-            player_ships.append(single_ship)
-            ships_dict[4] -=1
-    return player_ships
-
-
-def what_ships():
-    #creates variable with ship existing in game
-    pass
-
-def ship_placement():
-    pass
-
-
-def shooting_phase():
-    pass
-
-def ask_for_shot():
-    pass
-
-def is_shot_valid():
-    pass
-
-def hit_or_miss():
-    pass
-
-def is_ship_sunk():
-    pass
-
-def win_condition():
-    pass
-
-player1_board = get_empty_board()
-player2_board = get_empty_board()
-player1_shoots = get_empty_board()
-player2_shoots = get_empty_board()
+def get_direction():
+    direction = input('Please make a choice, if ship should go to the right , or downwards from your position (type R or D and press Enter): ')
+    while direction.upper != "R" or direction.upper != "D":
+        direction = input('Please make a choice, if ship should go to the right , or downwards from your position (type R or D and press Enter: ')
+    return direction.upper()
 
 
 
-
-
-
-def main_logic():
-    pass
+def get_ship_locations(player_board, ships_dict, board_size):
+    for ship_size in range(1, len(ships_dict)+1):
+        if ship_size == 1:
+            while ships_dict[1]>0:
+                ship_location = ask_for_input(ship_size)
+                if is_valid_input(ship_location, board_size) == True:
+                    coordinates = get_player_coordinates_format(ship_location) #changes player input like A1 to coordinates like '00'
+                    if check_if_position_empty(ship_location, player_board) == True and check_if_adjecent_empty(player_board, ship_location)==True:
+                        player_board.append(coordinates)
+                        ships_dict[1] = ships_dict[1]-1
