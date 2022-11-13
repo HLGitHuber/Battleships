39813e86028b082e75dafea705328dd879b72389
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2022-11-13 12:18:17 +0100

    Znalezienie odpowiednich argumentów
    Zamieniłem argumenty w funkcjach, teraz sprawdzają odpowiednie rzeczy

--- battleship.py
+++ battleship.py
@@ -1,30 +1,30 @@
 shots = []
 board = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 ships_start = []
-ships = []
-
+ships = ["A1"]
 
 def ask_for_shot():
-    shot = input('Choose the place to shoot')
+    shot = input('Choose the place to shoot ')
     return shot
 
-def is_shot_valid(shot):
-    if shot not in board:
+def is_shot_valid(ask_for_shot):
+    if ask_for_shot not in board:
         print ("Your shot is outside the board, try again")
-        is_shot_valid(shot)
-    elif shot in shots:
+        ask_for_shot()
+    elif ask_for_shot in shots:
         print ("You have already shot there, try again")
-        is_shot_valid(shot)
+        ask_for_shot()
     else :
-        shots.append(shot)
+        shots.append(ask_for_shot)
         return True
 
 
-def hit_or_miss(shot):
-    if shot in ships:
+def hit_or_miss(ask_for_shot):
+    if ask_for_shot in ships:
         print("You hit enemy ship!")
-        ships.remove(shot)
+        ships.remove(ask_for_shot)
     else:
         print('You missed the shot')
+
 
 
