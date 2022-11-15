75712d439a622fadc2f7e5e2ffb71c619c9c2797
arkadiuszwoznica <115310756+arkadiuszwoznica@users.noreply.github.com>
2022-11-15 12:50:32 +0100

    Valid shot and hit_or_miss final version
    Dopracowałem funkcje asa_for_shot, is_shot_valid I hit_or_miss. W tym momencie wszystko działa jak powinno

--- battleship.py
+++ battleship.py
@@ -1,30 +1,38 @@
-shots = []
+shots = ["B2"]
 board = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 ships_start = []
 ships = ["A1"]
 
 def ask_for_shot():
+    global shot
     shot = input('Choose the place to shoot ')
     return shot
 
+
 def is_shot_valid(ask_for_shot):
-    if ask_for_shot not in board:
-        print ("Your shot is outside the board, try again")
-        ask_for_shot()
-    elif ask_for_shot in shots:
+    while shot not in board:
+        print ("Your shot is not valid, try again")
+        is_shot_valid(ask_for_shot)
+    while shot in shots:
         print ("You have already shot there, try again")
-        ask_for_shot()
-    else :
-        shots.append(ask_for_shot)
+        is_shot_valid(ask_for_shot)
+    while shot in board and shot not in shots :
+        shots.append(shot)
         return True
 
 
 def hit_or_miss(ask_for_shot):
-    if ask_for_shot in ships:
+    if shot in ships:
         print("You hit enemy ship!")
-        ships.remove(ask_for_shot)
+        ships.remove(shot)
     else:
         print('You missed the shot')
+        
 
-
-
+ask_for_shot()
+print(shots)
+print(ships)
+print(is_shot_valid(ask_for_shot))
+print(shots)
+hit_or_miss(ask_for_shot)
+print(ships)
