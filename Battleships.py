ec9f32759bb5e926d72946ed622eac06651eeb1f
HLGitHuber <hubertlewandowski2002@gmail.com>
2022-11-16 16:55:14 +0100

    major changes code put together from others

--- Battleships.py
+++ Battleships.py
@@ -1,10 +1,11 @@
 #BATTLESHIP GAME
 
-def placement_phase(player):
-    pass
+def get_empty_board(board_size):
+    return  [['O' for _ in range(board_size)] for _ in range(board_size)]
 
-def get_empty_board(size):
-    return  [['O' for _ in range(size)] for _ in range(size)]
+def ask_for_input(ship_size):
+    coordinates = input(f"Please provide position in format ie. A1, size of current ship is {ship_size}: ")
+    return coordinates
 
 def display_board(game_board):
     alphabet = 'ABCDEFGHIJ'
@@ -14,51 +15,145 @@ def display_board(game_board):
     for x in range(0, (display_size)):
         print(f'{alphabet[x]} {" ".join(game_board[x])}')
 
-# is ship placement valid
-def valid_input():
-    pass
+def is_valid_input(player_input, size):
+    if len(player_input) ==2:
+        rows = player_input[0]
+        columns = player_input[1]
+        coordinates = (rows, columns)
+        rows = coordinates[0]
+        try:
+            columns = int(coordinates[1])
+        except ValueError:
+            columns = None
+        if rows.upper()  in ['A','B','C','D', 'E', 'F', 'G','H', 'I', 'J'] and columns in range(1,size+1):
+            return True
+    else:
+        return False
+    
+def check_if_position_empty(player_coordinates, player_board):
+    try:
+        if player_board[int(player_coordinates[0])][int(player_coordinates[1])] == 'O':
+            return True
+        else:
+            return False
+    except IndexError:
+        return False
 
-def ship_proximity():
-    #horizontal or ventical, regardless of corners
-    pass
+def get_player_coordinates_format(player_coordinate):
+    rows_conversion_dict = {"A": 0, "B": 1, "C": 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6,'H': 7, 'I': 8, 'J': 9}
+    row = rows_conversion_dict[player_coordinate[0].upper()]
+    column = int(player_coordinate[1])-1
+    player_coordinates_format = f"{str(row)}{str(column)}"
+    return player_coordinates_format
 
-def what_ships():
-    #creates variable with ship existing in game
-    pass
+def get_valid_input(player_coordinates, player_board, ship_size): 
+    player_coordinates = ask_for_input(ship_size)
+    while check_if_position_empty(player_coordinates, player_board) == False or is_valid_input(player_coordinates, ship_size) == False:
+        player_coordinates = ask_for_input(ship_size)
+        if check_if_position_empty(player_coordinates, player_board) == True and is_valid_input(player_coordinates, ship_size) == True:
+            return get_player_coordinates_format(player_coordinates)
 
-def ship_placement():
-    pass
+def check_if_adjecent_empty(player_board, ship_location):
+    rows = int(ship_location[0])
+    columns = int(ship_location[1])
+    try:
+        if player_board[rows][columns-1] == 'O': #sprawdza w lewo
+            if player_board[rows][columns+1] == 'O': #sprawdza w prawo
+                if player_board[rows+1][columns] == 'O': #sprawdza na górze
+                    if player_board[rows-1][columns] == 'O': #sprawdza na dole
+                        return True
+        return False
+    except IndexError:
+        return True
+
+def get_direction():
+    direction = input('Please make a choice, if ship should go to the right , or downwards from your position (type R or D and press Enter): ')
+    while direction.upper != "R" or direction.upper != "D":
+        direction = input('Please make a choice, if ship should go to the right , or downwards from your position (type R or D and press Enter: ')
+    return direction.upper()
+
+def get_ship_locations(player_board, ships_dict, board_size):
+    for ship_size in range(1, len(ships_dict)+1):
+        if ship_size == 1:
+            while ships_dict[1]>0:
+                ship_location = ask_for_input(ship_size)
+                if is_valid_input(ship_location, board_size) == True:
+                    coordinates = get_player_coordinates_format(ship_location) #changes player input like A1 to coordinates like '00'
+                    if check_if_position_empty(coordinates, player_board) == True and check_if_adjecent_empty(player_board, coordinates)==True:
+                        player_board[int(coordinates[0])][int(coordinates[1])] = 'X'
+                        # player_ships.append(coordinates)
+                        ships_dict[1] = ships_dict[1]-1
+                else:
+                    print('This seems to be a wrong coordinate')
 
 
-def shooting_phase():
-    pass
+
+BOARD_SIZE = 5
+
+# board_player1 = [['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O'], ['O', 'O', 'O', 'O', 'O']]
+board_player1 = get_empty_board(BOARD_SIZE)
+board_player2 = get_empty_board(BOARD_SIZE)
+shooting_board_player1 = get_empty_board(BOARD_SIZE)
+shooting_board_player2 = get_empty_board(BOARD_SIZE)
+
+player1_ships = [['00'], ['44'], ['22','23'], ['30', '40', '20']]
+
+ships_dict = {1:1, 2:0, 3:0, 4:0}
 
 def ask_for_shot():
-    pass
+    global shot
+    shot = input('Choose the place to shoot: ')
+    return shot
 
-def is_shot_valid():
-    pass
+def shooting_phase():
+    display_board(board_player1)
+    make_a_shot(player1_ships, board_player1)
+    display_board(board_player1)
+    make_a_shot(player1_ships, board_player1)
+    display_board(board_player1)
 
-def hit_or_miss():
-    pass
+def make_a_shot(player_ships, player_board):
+    x = True
+    while x:
+        shot = ask_for_shot()
+        if is_valid_input(shot, BOARD_SIZE):
+            shot = get_player_coordinates_format(shot)
+            for ship in range(len(player_ships)):
+                if shot in player_ships[ship]:
+                    player_board[int(shot[0])][int(shot[1])] = 'H'
+                    player_ships[ship].remove(shot)
+                    if len(player_ships[ship]) == 0:
+                        player_board[int(shot[0])][int(shot[1])] = 'S'
+                        rows = int(shot[0])
+                        columns = int(shot[1])
+                        if player_board[rows][columns-1] == 'H': #sprawdza w lewo
+                            player_board[rows][columns-1] = 'S'
+                        elif player_board[rows][columns+1] == 'H': #sprawdza w prawo
+                            player_board[rows][columns+1] = 'S'
+                        elif player_board[rows+1][columns] == 'H': #sprawdza na górze
+                            player_board[rows+1][columns] = 'S'
+                        elif player_board[rows-1][columns] == 'H': #sprawdza na dole
+                            player_board[rows-1][columns] = 'S'
 
-def is_ship_sunk():
-    pass
-
-def win_condition(player_board):
-    board_size = len(player_board)
-    for row in range(board_size):
-        for col in range(board_size):
-            if player_board[row][col] == 'X':
-                return False
-    return True
-
-player1_board = get_empty_board(5)
-player2_board = get_empty_board(5)
-player1_shoots = get_empty_board(5)
-player2_shoots = get_empty_board(5)
+                        print("You've sunk a ship!")
+                        return
+                    print("You've hit a ship!")
+                    return
+                
+            player_board[int(shot[0])][int(shot[1])] = 'M'
+            print("You've missed!")
+            x = False
+            return 
+        else:
+            print("Incorrect coordinates")
 
 
-def main_logic():
-    pass
+shooting_phase()
+
+
+    
+
+
+# remove z listy player_ships gdy trafi statek, jezeli 0 w tej liscie to zatopiony
+# albo sprawdza boarda jezeli w poblizu nie ma kolejnego X to zatopiony
 
